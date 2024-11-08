21414fc2018b6d4cbc72a39e01679934485d471a
Hendra Pratama <hendrapratama12@gmail.com>
2024-11-08 13:07:43 +0700

    tes commit

--- Front-End/src/index.jsx
+++ Front-End/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// scroll bar
+// scroll bar tes
 import 'simplebar-react/dist/simplebar.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
